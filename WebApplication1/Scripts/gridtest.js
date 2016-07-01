ec8ffb004b8efef54e108b2b2c5e8119cfc569f2
Cristian Santilli <cristiansantilli@gmail.com>
2016-06-30 22:43:53 -0300

    Changed grid test to be able to unsort all the columns. Still no luck.

--- WebApplication1/Scripts/gridtest.js
+++ WebApplication1/Scripts/gridtest.js
@@ -1,25 +1,29 @@
 ﻿var app = angular.module('app', ['ui.grid']);
 
-app.controller('MainCtrl', ['$scope', function ($scope) {
+app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
-    $scope.myData = [
-      {
-          "firstName": "Cox",
-          "lastName": "Carney",
-          "company": "Enormo",
-          "employed": true
-      },
-      {
-          "firstName": "Lorraine",
-          "lastName": "Wise",
-          "company": "Comveyer",
-          "employed": false
-      },
-      {
-          "firstName": "Nancy",
-          "lastName": "Waters",
-          "company": "Fuelton",
-          "employed": false
-      }
+    $scope.showMe = function () {
+        //$scope.gridOptions.data = $scope.firstData;        
+    };
+
+    $scope.gridOptions = {};
+    $scope.gridOptions.columnDefs = [
+         { name: 'name' },
+         { name: 'gender' },
+         {
+             name: 'ShowScope',
+             cellTemplate: '<button class="btn primary" ng-click="grid.appScope.showMe()">Click Me</button>'
+         }
     ];
+
+    $scope.gridOptions.onRegisterApi = function (gridApi) {
+        //set gridApi on scope
+        $scope.gridApi = gridApi;
+        $scope.gridApi.core.on.sortChanged($scope, function (grid, sortColumns) { while (sortColumns.length > 1) { sortColumns[0].unsort(); sortColumns.shift(); } });
+    };
+
+    $http.get('http://ui-grid.info/data/100.json')
+      .success(function (data) {
+          $scope.gridOptions.data = data;
+      });
 }]);
